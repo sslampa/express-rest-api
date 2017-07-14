f995467c863c0ca62e7d96ae4526275587d598f2
sslampa <sslampa@gmail.com>
2017-07-14 10:02:37 -0700

    Serve static files

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+
 
 app.get('/', function(req, res) {
     res.send('Hello World!')
 })
+
+app.use('/static', express.static(path.join(__dirname, 'assets')))
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')

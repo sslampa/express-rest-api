03355f109082dacfca56cbe63f1b35f497f6ebb3
sslampa <sslampa@gmail.com>
2017-07-14 23:18:59 -0700

    Add testing libraries

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const app = express()
+const port = 8080
 
 
 app.get('/api/hello-world', (req, res) => {
-    res.send({first: "Hello", second: "World"})
+    res.json({first: "Hello", second: "World"})
 })
 
 app.get('/', (req, res) => {
     res.send('This is the opening page')
 })
 
-app.listen(8000, () => {
-    console.log("Listening on port: " + 8000)
+app.listen(port, () => {
+    console.log("Listening on port: " + port)
 })
-

659fb777e5fd3cb918f6f98b15a8853c944bf101
sslampa <sslampa@gmail.com>
2017-07-21 18:33:06 -0700

    Make first API request

--- src/App/script.js
+++ src/App/script.js
@@ -1,6 +1,18 @@
+import axios from 'axios'
+
 export default {
   name: 'App',
-  data() {
-    return {}
-  },
+  data: () => ({
+    first: "",
+    second: ""
+  }),
+
+  created() {
+    axios.get(`http://localhost:8080/api/hello-world`)
+    .then(response => {
+      let data = response.data
+      this.first = data.first
+      this.second = data.second
+    })
+  }
 }
